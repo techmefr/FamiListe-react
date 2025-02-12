69a7853441bb4cc2f29d05b724825af0915e735d
techmefr <compigni.gaetan@gmail.com>
2025-02-12 18:51:09 +0100

    feat(components): add proptypes to base components

--- client/src/components/typography/Heading.jsx
+++ client/src/components/typography/Heading.jsx
@@ -1,7 +1,20 @@
-function Heading({ children, level = 1, className = '' }) {
+import PropTypes from 'prop-types';
+
+function Heading({ children, level, className }) {
   const Tag = `h${level}`;
 
   return <Tag className={`heading heading-${level} ${className}`}>{children}</Tag>;
 }
+
+Heading.propTypes = {
+  children: PropTypes.node.isRequired,
+  level: PropTypes.oneOf([1, 2, 3, 4, 5, 6]),
+  className: PropTypes.string,
+};
+
+Heading.defaultProps = {
+  level: 1,
+  className: '',
+};
 
 export default Heading;
--- client/src/components/typography/Test.jsx
+++ client/src/components/typography/Test.jsx
@@ -1,5 +1,18 @@
-function Text({ children, variant = 'body', className = '' }) {
+import PropTypes from 'prop-types';
+
+function Text({ children, variant, className }) {
   return <p className={`text ${variant} ${className}`}>{children}</p>;
 }
+
+Text.propTypes = {
+  children: PropTypes.node.isRequired,
+  variant: PropTypes.oneOf(['body', 'small']),
+  className: PropTypes.string,
+};
+
+Text.defaultProps = {
+  variant: 'body',
+  className: '',
+};
 
 export default Text;
